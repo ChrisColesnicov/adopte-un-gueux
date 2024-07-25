2b08291d6515cbb57cc82bfed9f6231bf90cb1b4
Davido91390 <david.kabore@hotmail.fr>
2024-07-25 15:09:31 +0200

    add link to onboarding in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
 import "../styles/footer.css";
 
 export default function Footer() {
   return (
     <section className="footer">
       <div className="footer-img">
-        <img src="../src/assets/images/onboarding.svg" alt="Go to Onboarding" />
+        <Link to="/onboard">
+          <img
+            src="../src/assets/images/onboarding.svg"
+            alt="Go to Onboarding"
+          />
+        </Link>
         <img src="../src/assets/images/chat.svg" alt="Go to chat" />
         <img
           src="../src/assets/images/cart.svg"
