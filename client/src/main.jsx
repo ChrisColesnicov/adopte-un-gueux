5453cbd7f872112b73fb83adaac2571d6cc08c6b
Rossignol Alex <alexnw33910@gmail.com>
2024-07-25 23:44:37 +0200

    Ajout du composent userDescriptif & de ses routes & features

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,9 @@ import getProfil from "./services/profils";
 
 import App from "./App";
 import Accueil from "./pages/Accueil";
-import Chat from "./pages/Chat";
 import Cart from "./pages/Cart";
 import CartProvider from "./contexts/CartContext";
+import UserDescriptif from "./components/UserDescriptif";
 
 const router = createBrowserRouter([
   {
@@ -20,8 +20,9 @@ const router = createBrowserRouter([
         loader: getProfil,
       },
       {
-        path: "/chat",
-        element: <Chat />,
+        path: "/:id",
+        element: <UserDescriptif />,
+        loader: getProfil,
       },
       {
         path: "/cart",
@@ -40,3 +41,42 @@ root.render(
     </CartProvider>
   </React.StrictMode>
 );
+
+// import React from "react";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import ReactDOM from "react-dom/client";
+// import getProfil from "./services/profils";
+
+// import App from "./App";
+// // import Accueil from "./pages/Accueil";
+// import Cart from "./pages/Cart";
+// import CartProvider from "./contexts/CartContext";
+// import UserDescriptif from "./components/UserDescriptif";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <UserDescriptif />,
+//         loader: getProfil,
+//       },
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//     ],
+//   },
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   <React.StrictMode>
+//     <CartProvider>
+//       <RouterProvider router={router} />
+//     </CartProvider>
+//   </React.StrictMode>
+// );
