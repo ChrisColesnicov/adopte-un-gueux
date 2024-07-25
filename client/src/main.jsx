c93189b958ff94660a1e37c1df896c497f2bee09
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-07-25 15:39:47 +0200

    created cart context and map in cart page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Accueil from "./pages/Accueil";
 import Cart from "./pages/Cart";
+import CartProvider from "./contexts/CartContext";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <CartProvider>
     <RouterProvider router={router} />
+    </CartProvider>
   </React.StrictMode>
 );
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,3 +1,16 @@
-export default function Cart () {
-    return <h2>panier de gueux</h2>
+import profilesList from "../services/profilsData";
+
+export default function Cart() {
+  const cart = profilesList;
+
+  return (
+    <section>
+      <h2>Panier de gueux</h2>
+      {cart.map((profile) => (
+        <div key={profile.id}>
+          <p>{profile.prenom}<span> {profile.nom}</span></p>
+        </div>
+      ))}
+    </section>
+  );
 }
