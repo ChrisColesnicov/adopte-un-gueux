be57e7ad7a9cffb41b1b562505ab9b6eed1f4a26
Davido91390 <david.kabore@hotmail.fr>
2024-07-25 22:07:29 +0200

    css modificatioin

--- client/src/pages/Onboarding.jsx
+++ client/src/pages/Onboarding.jsx
@@ -10,154 +10,170 @@ export default function Onboarding() {
     <section className="onboarding">
       <h3>Par quel noms on vous connaît en ce pays ?</h3>
       <form>
-        <input type="text" className="name" />
+        <input type="text" className="name" placeholder="Noms" />
       </form>
       <h3>De quel genre êtes-vous ?</h3>
       <div className="gender">
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Homme"
-            onChange={handleChange}
-          />
-          Homme
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Homme"
-            onChange={handleChange}
-          />{" "}
-          Femme
-        </label>
+        <div className="gender-1">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Homme"
+              onChange={handleChange}
+            />{" "}
+            Homme
+          </label>
+        </div>
+        <div className="gender-2">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Homme"
+              onChange={handleChange}
+            />{" "}
+            Femme
+          </label>
+        </div>
       </div>
       <h3>De quel genre recherchez-vous ?</h3>
       <div className="search-gender">
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Homme"
-            onChange={handleChange}
-          />{" "}
-          Homme
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Homme"
-            onChange={handleChange}
-          />{" "}
-          Femme
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name=" SCousin/Cousine"
-            onChange={handleChange}
-          />
-          Cousin/Cousine
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Chèvre"
-            onChange={handleChange}
-          />{" "}
-          Chèvre
-        </label>
+        <div className="one-gender">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Homme"
+              onChange={handleChange}
+            />{" "}
+            Homme
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Homme"
+              onChange={handleChange}
+            />{" "}
+            Femme
+          </label>
+        </div>
+        <div className="two-gender">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name=" SCousin/Cousine"
+              onChange={handleChange}
+            />{" "}
+            Cousin/Cousine
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Chèvre"
+              onChange={handleChange}
+            />{" "}
+            Chèvre
+          </label>
+        </div>
       </div>
       <h3>Quel type de relation ?</h3>
       <div className="relation">
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Mariage forcé"
-            onChange={handleChange}
-          />{" "}
-          Mariage forcé
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Familiale"
-            onChange={handleChange}
-          />{" "}
-          Familiale
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Jout d'une nuit"
-            onChange={handleChange}
-          />{" "}
-          Jout d'une nuit
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Libertine"
-            onChange={handleChange}
-          />{" "}
-          Libertine
-        </label>
+        <div className="one-relation">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Mariage forcé"
+              onChange={handleChange}
+            />{" "}
+            Mariage forcé
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Familiale"
+              onChange={handleChange}
+            />{" "}
+            Familiale
+          </label>
+        </div>
+        <div className="two-relation">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Jout d'une nuit"
+              onChange={handleChange}
+            />{" "}
+            Jout d'une nuit
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Libertine"
+              onChange={handleChange}
+            />{" "}
+            Libertine
+          </label>
+        </div>
       </div>
       <h3>Quel type de rang social ?</h3>
       <div className="rang-social">
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Noblesse"
-            onChange={handleChange}
-          />{" "}
-          Noblesse
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Bourgeoise"
-            onChange={handleChange}
-          />{" "}
-          Clergé
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Marginaux"
-            onChange={handleChange}
-          />{" "}
-          Marginaux
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Clergé"
-            onChange={handleChange}
-          />{" "}
-          Bourgoise
-        </label>
-        <label>
-          <input
-            className="checkbox"
-            type="checkbox"
-            name="Paysannerie"
-            onChange={handleChange}
-          />{" "}
-          Paysannerie
-        </label>
+        <div className="rang-1">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Noblesse"
+              onChange={handleChange}
+            />{" "}
+            Noblesse
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Bourgeoise"
+              onChange={handleChange}
+            />{" "}
+            Clergé
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Marginaux"
+              onChange={handleChange}
+            />{" "}
+            Marginaux
+          </label>
+        </div>
+        <div className="rang-2">
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Clergé"
+              onChange={handleChange}
+            />{" "}
+            Bourgoise
+          </label>
+          <label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Paysannerie"
+              onChange={handleChange}
+            />{" "}
+            Paysannerie
+          </label>
+        </div>
       </div>
       <button type="button" className="button-onboarding">
         VALIDER
