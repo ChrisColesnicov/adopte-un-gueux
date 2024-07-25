0cae69869ac44d663e8f260f667f9281063c8a2b
Davido91390 <david.kabore@hotmail.fr>
2024-07-25 14:09:23 +0200

    Footer modification and add css to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,14 @@
+import "../styles/footer.css";
+
 export default function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-      <p>coucou</p>
-    </>
+    <section className="footer">
+      <div className="footer-img">
+        <img src="../src/assets/images/onboarding.svg" alt="" />
+        <img src="../src/assets/images/chat.svg" alt="Go to chat" />
+        <img src="../src/assets/images/cart.svg" alt="" />
+        <img src="../src/assets/images/parameters.svg" alt="Go to parameters" />
+      </div>
+    </section>
   );
 }
