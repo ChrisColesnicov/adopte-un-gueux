39384d34944b5a94cca2085cceb418cd2c5e7fd6
Davido91390 <david.kabore@hotmail.fr>
2024-07-25 14:51:38 +0200

    add alt in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,9 +4,12 @@ export default function Footer() {
   return (
     <section className="footer">
       <div className="footer-img">
-        <img src="../src/assets/images/onboarding.svg" alt="" />
+        <img src="../src/assets/images/onboarding.svg" alt="Go to Onboarding" />
         <img src="../src/assets/images/chat.svg" alt="Go to chat" />
-        <img src="../src/assets/images/cart.svg" alt="" />
+        <img
+          src="../src/assets/images/cart.svg"
+          alt="Your cart - Favorite people"
+        />
         <img src="../src/assets/images/parameters.svg" alt="Go to parameters" />
       </div>
     </section>
