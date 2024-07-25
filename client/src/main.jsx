f7e026b863dabf4e53818dd74a68853b4b85bb0f
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-07-25 14:55:59 +0200

    created onboarding page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import Accueil from "./pages/Accueil";
+import Onboarding from "./pages/Onboarding";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Accueil />,
+      },
+      {
+        path: "/onboard",
+        element: <Onboarding />,
       },
     ]
     }
---
+++ client/src/pages/Onboarding.jsx
@@ -0,0 +1,3 @@
+export default function Onboarding() {
+    return <h2>A bord les gueux !</h2>
+}
