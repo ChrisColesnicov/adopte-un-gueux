4af7f280905cbc638c4d16c5558a32c638787f7b
Rossignol Alex <alexnw33910@gmail.com>
2024-07-25 21:27:37 +0200

    delete code mort

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,13 @@
-import { Outlet, useLoaderData } from "react-router-dom";
-import Papa from "papaparse";
+import { Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import "./styles/App.css";
 import "./styles/accueil.css";
-import "./styles/cart.css";
+import "./styles/cart.css"
 
 function App() {
-  const dataFromLoader = useLoaderData();
-
-  const parse = () =>
-    Papa.parse(dataFromLoader.data, {
-      header: true,
-      complete: (result) => result,
-    });
-  const { data } = parse();
-  console.info(data);
   return (
     <>
       <Navbar />
