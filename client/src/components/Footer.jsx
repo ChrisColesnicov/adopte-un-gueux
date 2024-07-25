c743e5fe1fa28be53f4130157c63dcd641e7652f
dgonc <d.goncalves89000@gmail.com>
2024-07-25 20:34:21 +0200

    add link to cart and chat

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "../styles/footer.css";
 
 export default function Footer() {
@@ -5,11 +7,15 @@ export default function Footer() {
     <section className="footer">
       <div className="footer-img">
         <img src="../src/assets/images/onboarding.svg" alt="Go to Onboarding" />
-        <img src="../src/assets/images/chat.png" alt="Go to chat" />
-        <img
-          src="../src/assets/images/cart.png"
-          alt="Your cart - Favorite people"
-        />
+        <Link to="/chat">
+          <img src="../src/assets/images/chat.png" alt="Go to chat" />
+        </Link>
+        <Link to="/cart">
+          <img
+            src="../src/assets/images/cart.png"
+            alt="Your cart - Favorite people"
+          />
+        </Link>
         <img src="../src/assets/images/parameters.svg" alt="Go to parameters" />
       </div>
     </section>
